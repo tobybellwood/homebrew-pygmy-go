44ed2e9199ebbff6985a8547f93a58d6d514422f
Karl Hepworth <karl.hepworth@gmail.com>
2020-05-02 23:56:34 +1000

    build from source
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- Formula/pygmy-go.rb
+++ Formula/pygmy-go.rb
@@ -1,13 +1,19 @@
 class PygmyGo < Formula
   desc "Amazee.io's local development helper tool"
   homepage "https://github.com/fubarhouse/pygmy-go"
-  url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.2.0/pygmy-go-darwin"
-  sha256 "8d7780ad9183d613313140aab0e088a61919e7efff69c5d0ab09005c2671d67c"
+  url "https://github.com/fubarhouse/pygmy-go/archive/v0.2.0.tar.gz"
+  sha256 "26377fd6dc6e1b231cb7a5ee0e7d91dc1fbbf82450094ab578a867415e9f5580"
   version "v0.2.0"
 
+  depends_on "go" => :build
+
   def install
-    libexec.install Dir["*"]
-    chmod(0755, "#{libexec}/pygmy-go-darwin")
-    bin.install_symlink("#{libexec}/pygmy-go-darwin" => "pygmy")
+    ENV["GOPATH"] = buildpath
+    bin_path = buildpath/"src/github.com/fubarhouse/pygmy-go"
+    bin_path.install Dir["*"]
+    cd bin_path do
+      system "go", "build", "-o", bin/"pygmy", "-ldflags", "-X main.version=#{version}", "."
+    end
   end
+
 end
