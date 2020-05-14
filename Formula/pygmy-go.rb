35433db1110bf0a50a665556925b500e49f47b16
Karl Hepworth <karl.hepworth@gmail.com>
2020-05-14 21:16:11 +1000

    Update pygmy to 0.3.0
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- Formula/pygmy-go.rb
+++ Formula/pygmy-go.rb
@@ -1,9 +1,9 @@
 class PygmyGo < Formula
   desc "Amazee.io's local development helper tool"
   homepage "https://github.com/fubarhouse/pygmy-go"
-  url "https://github.com/fubarhouse/pygmy-go/archive/v0.2.0.tar.gz"
-  sha256 "26377fd6dc6e1b231cb7a5ee0e7d91dc1fbbf82450094ab578a867415e9f5580"
-  version "v0.2.0"
+  url "https://github.com/fubarhouse/pygmy-go/archive/v0.3.0.tar.gz"
+  sha256 "cbe411eb2fbc870440d859ad20855d0e73b1fade9f303836056140df8af7e88c"
+  version "v0.3.0"
 
   depends_on "go" => :build
 
