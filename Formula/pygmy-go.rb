9b970326028d201cb9540c8cf2c60cd1a473c270
Toby Bellwood <toby.bellwood@gmail.com>
2020-05-16 17:04:34 +1000

    made dual version linux/mac

--- Formula/pygmy-go.rb
+++ Formula/pygmy-go.rb
@@ -1,19 +1,24 @@
 class PygmyGo < Formula
   desc "Amazee.io's local development helper tool"
   homepage "https://github.com/fubarhouse/pygmy-go"
-  url "https://github.com/fubarhouse/pygmy-go/archive/v0.3.0.tar.gz"
-  sha256 "31965b17db3b1523fc7b238700a7b2d7fc7bff502b82d0c9df7364f0f1bd0f25"
   version "v0.3.0"
+  bottle :unneeded
 
-  depends_on "go" => :build
-
-  def install
-    ENV["GOPATH"] = buildpath
-    bin_path = buildpath/"src/github.com/fubarhouse/pygmy-go"
-    bin_path.install Dir["*"]
-    cd bin_path do
-      system "go", "build", "-o", bin/"pygmy-go", "-ldflags", "-X main.version=#{version}", "."
+  if OS.mac?
+    url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.0/pygmy-go-darwin"
+    sha256 "4907dcb42f5a9d3c2ff366b65c786a626a8e3e63a28ca6908914e5df4a966f9f"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.0/pygmy-go-linux-x86"
+      sha256 "71ae9623b0e16312d4bbd58e6d7c8487831a0e8b0420be0e9a515295c418b971"
     end
   end
 
+  def install
+    bin.install "pygmy-go"
+  end
+
+  test do
+    system "#{bin}/pygmy-go --version"
+  end
 end
